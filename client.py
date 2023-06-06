d834b4d6a44e871606d3e59901802078cdd555ec
Tetiana <tetiana.sliusarenko@nure.ua>
2023-06-06 20:00:25 +0300

    added command handling to client
    + fixed some errors with sending and receiving data
    now messenger is working in console

--- client.py
+++ client.py
@@ -1,17 +1,18 @@
 import socket
 import threading
-import threading as thr
-import time
 import json
-import sys
+import time
 import pandas as pd
 
 
 user = {'username': '', 'password': ''}
+messages = pd.DataFrame({'send': [], 'recv': [], 'message': []})
+online_users = []
 
 
+# MAIN FUNCTIONS THAT SEND AND RECEIVE MESSAGES
 # a function that sends our messages to a client
-def send_msg(data, soc):
+def deliver_msg(data, soc):
     soc.sendall(json.dumps(data).encode())
 
 
@@ -21,14 +22,100 @@ def recv_msg(soc):
     return json.loads(data.decode())
 
 
-def receive_msg():
+# FUNCTIONS THAT RESPONSIBLE FOR COMMANDS
+# functions that updates online users
+def update_users(data):
+    if data['status'] == 'add':
+        online_users.append(data['username'])
+        print('New user:', data['username'])
+    elif data['statue'] == 'remove':
+        online_users.remove(data['username'])
+        print('User disconnected:', data['username'])
+
+
+# functions that gets a checked data
+def get_checked_data(data):
+    if data['result']:
+        print('Everything is correct.')
+    else:
+        print(data['error'])
+        enter_data()
+
+
+# functions that gest a database
+def get_database(data):
+    d_type = data['type']
+    if d_type == 'msgs':
+        get_db_msg(data['res'])
+    elif d_type == 'users':
+        get_db_users(data['res'])
+
+
+# functions that gets a message and adds it to database
+def get_message(data):
+    send, recv, msg = data['send'], data['recv'], data['message']
+    messages.loc[len(messages)] = [send, recv, msg]
+    print(f'Message from {send}: {msg}.')
+
+
+# function for entering data
+def enter_data():
+    status_valid = False
+    status = ''
+
+    while not status_valid:
+        status = input('Login or sign up?').strip().lower()
+        if status == 'login' or status == 'sign up':
+            status_valid = True
+
+    user['username'] = input('Username: ')
+    user['password'] = input('Password: ')
+
+    user_data = {'command': 'check_data', 'status': status, 'username': user['username'], 'password': user['password']}
+    deliver_msg(user_data, client)
+
+
+# FUNCTIONS THAT HELP GET A DATABASE
+# functions that gets a database with messages
+def get_db_msg(data):
+    messages = pd.DataFrame(data)
+    print('All messages:\n', messages)
+
+
+# functions that gets a database with users
+def get_db_users(data):
+    online_users = data[:]
+    print('Online: ', online_users)
+
+
+# FUNCTIONS THAT HELP REQUEST A DATABASE
+# functions that request a database with messages
+def request_msg_db():
+    req_data = {'command': 'database', 'username': user['username'], 'type': 'msgs'}
+    deliver_msg(req_data, client)
+
+
+# functions that request a database with users
+def request_users_db():
+    req_data = {'command': 'database', 'username': user['username'], 'type': 'users'}
+    deliver_msg(req_data, client)
+
+
+def handle_server():
     while True:
         try:
             data = recv_msg(client)
 
-            if 'message' in data:
-                msg = data['message']
-                print(f'A messages was received: {msg}')
+            if data['command'] == 'checked_data':
+                get_checked_data(data)
+            elif data['command'] == 'user_update':
+                update_users(data)
+            elif data['command'] == 'database':
+                get_database(data)
+            elif data['command'] == 'message':
+                get_message(data)
+            else:
+                print('Unknown command!!!')
 
         except ConnectionResetError:
             print('Server is off.')
@@ -47,12 +134,19 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(HOST)  # подключаемся с помощью клиентского сокета к серверу
 print('Connected to', HOST)
 
+enter_data()
+
 # start a new thread for receiving data
-t = threading.Thread(target=receive_msg)
+t = threading.Thread(target=handle_server)
 t.start()
+
+request_users_db()
+time.sleep(1)
+request_msg_db()
 
 # send a message to a server
 while True:
+    who = input('who? ')
     msg = input('')
-    data = {'message': msg}
-    send_msg(data, client)
+    data = {'command': 'message', 'send': user['username'], 'recv': who, 'message': msg}
+    deliver_msg(data, client)
--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 import socket
 import threading
-import time
 import json
-import sys
 import pandas as pd
 
 
@@ -63,12 +61,35 @@ def password_is_valid(password):
         return 'ok'
 
 
+# sends checked data to a client
 def send_checked_data(data, sock, result=False, error=''):
     data['result'] = result
     data['error'] = error
     deliver_msg(sock, data)
 
 
+# FUNCTION THAT PREPARE DATABASES FOR SENDING TO CLIENT
+# prepare users for sending to a client
+def prepare_users(username):
+    res = []
+    for u, s in online_users.items():
+        if u != username:
+            res.append(u)
+    return res
+
+
+# prepare messages for sending to a client
+def prepare_msgs(username):
+    res = {'send': [], 'recv': [], 'message': []}
+    for i, r in messages.iterrows():
+        if r['send'] == username or r['recv'] == username:
+            res['send'].append(r['send'])
+            res['recv'].append(r['recv'])
+            res['message'].append(r['message'])
+    return res
+
+
+# MAIN FUNCTIONS THAT SEND AND RECEIVE MESSAGES
 # a function that delivers messages to clients
 def deliver_msg(recv, data):
     recv.sendall(json.dumps(data).encode())
@@ -80,10 +101,20 @@ def recv_msg(sock):
     return json.loads(data.decode())
 
 
+# FUNCTIONS THAT RESPONSIBLE FOR COMMANDS
+# a function that sends databases to our client
+def send_database(data, sock):
+    if data['type'] == 'users':
+        data['res'] = prepare_users(data['username'])
+    elif data['type'] == 'msgs':
+        data['res'] = prepare_msgs(data['username'])
+    deliver_msg(sock, data)
+
+
 # a function that adds new user to online list and (maybe) database
 def new_user(data, sock):
     online_users[data['username']] = sock
-    print(f'User {data["username"]} connected. Socket: {online_users[data["username"]]}')
+    print('Online:', online_users.keys())
     new_data = {'command': 'user_update', 'username': data['username'], 'status': 'add'}
     for u, s in online_users.items():
         if s != sock:
@@ -91,10 +122,14 @@ def new_user(data, sock):
 
 
 # a function that removes user from online list
-def remove_user(data):
-    online_users.pop(data['username'])
-    print(f'User {data["username"]} disconnected.')
-    new_data = {'command': 'user_update', 'username': data['username'], 'status': 'remove'}
+def remove_user(sock):
+    username = ''
+    for u, s in online_users.items():
+        if s == sock:
+            online_users.pop(u)
+            print(f'User {u} disconnected.')
+            username = u
+    new_data = {'command': 'user_update', 'username': username, 'status': 'remove'}
     for u, s in online_users.items():
         if s != sock:
             deliver_msg(s, new_data)
@@ -104,11 +139,16 @@ def remove_user(data):
 def check_data(data, sock):
     status, username, password = data['status'], data['username'], data['password']
     new_data = {'command': 'checked_data', 'result': False, 'status': status, 'error': ''}
-    if status == 'login':
+    if username in online_users.keys():
+        send_checked_data(new_data, sock, error='This user is already online!')
+    elif status == 'login':
         if login_is_correct(username, password):
             send_checked_data(new_data, sock, result=True)
-        send_checked_data(new_data, sock, error='Incorrect username or password!')
-    elif status == 'signup':
+            print(f'{username} has connected.')
+            new_user(data, sock)
+        else:
+            send_checked_data(new_data, sock, error='Incorrect username or password!')
+    elif status == 'sign up':
         res1 = username_is_valid(username)
         if res1 == 'ok':
             res2 = password_is_valid(password)
@@ -116,6 +156,8 @@ def check_data(data, sock):
                 if username_is_free(username):
                     send_checked_data(new_data, sock, result=True)
                     add_user(username, password)
+                    print(f'{username} has connected.')
+                    new_user(data, sock)
                 else:
                     send_checked_data(new_data, sock, error='This username is already taken.')
             else:
@@ -126,32 +168,35 @@ def check_data(data, sock):
 
 # a function that sends out message to another client
 def send_msg(data):
-    if data['recv'] in online_users:
+    send, recv, msg = data['send'], data['recv'], data['message']
+    messages.loc[len(messages)] = [send, recv, msg]
+    messages.to_csv('messages.csv')
+    if recv in online_users:
         deliver_msg(online_users[data['recv']], data)
-        print(f'Message: {data["message"]}, was sent from {data["send"]} to {data["recv"]}')
+        print(f'Message: {msg}, was sent from {send} to {recv}')
     else:
         print('This user was disconnected.')
 
 
 # a function that handles a client (receives and sends messages)
-def handle_client(sock, addr):
+def handle_client(sock):
     while True:
         try:
             data = recv_msg(sock)
 
-            if data['command'] == 'user_data':
-                new_user(data, sock)
-            elif data['command'] == 'message':
+            if data['command'] == 'message':
                 send_msg(data)
             elif data['command'] == 'log_out':
                 remove_user(data)
             elif data['command'] == 'check_data':
                 check_data(data, sock)
+            elif data['command'] == 'database':
+                send_database(data, sock)
             else:
                 print('Unknown command!!!')
 
         except ConnectionResetError:
-            print(f'Client {addr} disconnected.')
+            remove_user(sock)
             break
 
     sock.close()
@@ -166,15 +211,10 @@ server.listen(5)
 
 print('Server is up and waiting for clients...')
 
-clients = []
-
 while True:
     # accept the client
     sock, addr = server.accept()
-    print(f'A client connected: {addr}')
-
-    clients.append(sock)
 
     # start a thread for our client
-    t = threading.Thread(target=handle_client, args=(sock, addr))
+    t = threading.Thread(target=handle_client, args=(sock,))
     t.start()
