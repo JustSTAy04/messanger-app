740dc52df377a636ad53d217c98708776baa01a8
Tetiana <tetiana.sliusarenko@nure.ua>
2023-06-06 13:23:20 +0300

    set up message passing between client and server

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 import threading as thr
 import time
 import json
@@ -37,20 +38,47 @@ def add_user(username, password):
 
 # a function that sends our messages to a client
 def send_msg(data, soc):
-    soc.send(json.dumps(data).encode())
+    soc.sendall(json.dumps(data).encode())
 
 
 # a function that receives a message from a client
 def recv_msg(soc):
     data = soc.recv(4096)
-    key = json.loads(data.decode())
+    return json.loads(data.decode())
+
+
+def receive_msg():
+    while True:
+        try:
+            data = recv_msg(client)
+
+            if 'message' in data:
+                msg = data['message']
+                print(f'A messages was received: {msg}')
+
+        except ConnectionResetError:
+            print('Server is off.')
+            close_socket()
+
+
+# closes our socket
+def close_socket():
+    client.close()
 
 
 HOST = (socket.gethostname(), 10000)
 
+# create a socket and connect to a server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(HOST)  # подключаемся с помощью клиентского сокета к серверу
 print('Connected to', HOST)
 
-resp = client.recv(4096)
-print(json.loads(resp.decode()))
+# start a new thread for receiving data
+t = threading.Thread(target=receive_msg)
+t.start()
+
+# send a message to a server
+while True:
+    msg = input('')
+    data = {'message': msg}
+    send_msg(data, client)
--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import sys
 import pandas as pd
 
 
-online_users = {'username': [], 'socket': []}
+online_users = {}
 
 # get the users' info from the .csv file and creates a dataframe (our database)
 users = pd.read_csv('users.csv', index_col=0)
@@ -17,30 +17,65 @@ messages = pd.read_csv('messages.csv', index_col=0)
 HOST = (socket.gethostname(), 10000)
 HEADER_SIZE = 10
 
-client_commands = ['user_data', 'message', 'log_out']
+client_commands = ['user_data', 'message', 'log_out', 'check_data']
 server_commands = ['message_database', 'message', 'log_out']
 
 
 # a function that sends our messages to a client
-def send_msg(data, soc):
-    soc.send(json.dumps(data).encode())
+def send_msg(data, sock):
+    sock.sendall(json.dumps(data).encode())
 
 
 # a function that receives a message from a client
-def recv_msg(soc):
-    data = soc.recv(4096)
-    key = json.loads(data.decode())
+def recv_msg(sock):
+    data = sock.recv(4096)
+    return json.loads(data.decode())
 
 
+# a function that sends out message to another client
+def deliver_message(msg, sock):
+    data = {'message': msg}
+    for client in clients:
+        if client != sock:
+            send_msg(data, client)
+
+
+# a function that handles a client (receives and sends messages)
+def handle_client(sock, addr):
+    while True:
+        try:
+            data = recv_msg(sock)
+
+            if 'message' in data:
+                msg = data["message"]
+                print(f'A message was received from {addr}: {msg}')
+                deliver_message(msg, sock)
+
+        except ConnectionResetError:
+            print(f'Client {addr} disconnected.')
+            break
+
+    sock.close()
+
+
+# create a server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(HOST)
 
-server.listen()
+server.listen(5)
+
+print('Server is up and waiting for clients...')
+
+clients = []
 
 while True:
-    conn, addr = server.accept()
-    print('Connected -', addr)
-    send_msg('Hi', conn)
-    print(f'Sent "Hi" to', conn, addr)
-    conn.close()
+    # accept the client
+    sock, addr = server.accept()
+    print(f'A client connected: {addr}')
+
+    clients.append(sock)
+
+    # start a thread for our client
+    t = threading.Thread(target=handle_client, args=(sock, addr))
+    t.start()
